198a6ff2f0a47a5563bdf6939ca3c7e3e391d4b2
xiaobing <yangxiaobing@baidu.com>
2017-08-24 17:32:32 +0800

    添加录音格式测试代码

--- XXBVideoRecoder/AVFoundation/XXBAVRecoder.m
+++ XXBVideoRecoder/AVFoundation/XXBAVRecoder.m
@@ -65,6 +65,9 @@
                                      AVEncoderBitRateKey: @16,
                                      AVNumberOfChannelsKey: @1,
                                      AVSampleRateKey: @(SAMPLE_RATE)};
+    
+
+    
     NSError *error;
     _audioRecorder = [[AVAudioRecorder alloc]
                       initWithURL:soundFileURL
@@ -74,4 +77,23 @@
         NSLog(@"XXB | %s [Line %d] %@ \nerror:%@",__func__,__LINE__,[NSThread currentThread],error);
     }
 }
+
+
+- (void)test {
+    //录音设置
+    NSMutableDictionary *recordSetting  = [[NSMutableDictionary alloc] init];
+    //设置录音格式  AVFormatIDKey==kAudioFormatLinearPCM
+    [recordSetting setValue:[NSNumber numberWithInt:kAudioFormatMPEG4AAC] forKey:AVFormatIDKey];
+    //设置录音采样率(Hz) 如：AVSampleRateKey==8000/44100/96000（影响音频的质量）
+    [recordSetting setValue:[NSNumber numberWithFloat:44100] forKey:AVSampleRateKey];
+    //录音通道数  1 或 2
+    [recordSetting setValue:[NSNumber numberWithInt:1] forKey:AVNumberOfChannelsKey];
+    //线性采样位数  8、16、24、32
+    [recordSetting setValue:[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
+    //录音的质量
+    [recordSetting setValue:[NSNumber numberWithInt:AVAudioQualityHigh] forKey:AVEncoderAudioQualityKey];
+    //是否使用浮点数采样
+    [recordSetting setValue:@(YES) forKey:AVLinearPCMIsFloatKey];
+}
+
 @end
